3dc03fbd91c144aa0ad81a0d504a8c6c00cfb763
PavelS <test11@mail.ru>
2022-05-08 21:12:13 +0300

    first learn app v/1.1.0.0

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const numberofFilms = +prompt('Сколько фильмов вы уже посмотрели?', '' );
 
 const personalMovieDB = {
@@ -8,13 +9,48 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Одиy из последних просмотренных фильмов?', '');
-const b = prompt('На сколько оцените его', '');
-const c = prompt('Оди из последних просмотренных фильмов?', '');
-const d = prompt('На сколько оцените его', '');
+////////////////////////////////////////////////////////////////////
+// const a = prompt('Один из последних просмотренных фильмов?', '');
+// const b = prompt('На сколько оцените его', '');
+// const c = prompt('Один из последних просмотренных фильмов?', '');
+// const d = prompt('На сколько оцените его', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+////////////////////////////////////////////////////////////////////
+
+
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его', '');
+
+    if (a != null && b != null && a !='' && b !='') {
+        personalMovieDB.movies[a] = b;     
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+
+}
+
 
 console.log(personalMovieDB);
+console.log(personalMovieDB.movies);
+
+
+if (personalMovieDB.count < 10) {
+    console.log('Посмотренно довольно мало фильмов, фильмов просмотренно: ' + personalMovieDB.count);
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log ('Вы класический зритель, фильмов просмотренно: ' + personalMovieDB.count);
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман, фильмов просмотренно: ' + personalMovieDB.count);
+} else {
+    console.log('произошла ошибка');
+}
+
+console.log(personalMovieDB.count[a]);
+console.log(personalMovieDB.count[b]);
+
 
